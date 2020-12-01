45aa29f75b9c7b5b204516c2a6016cb83375783b
Francisco Javier Garcia Muñoz <javidesoft@gmail.com>
2020-12-01 23:21:05 +0100

    Routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './routes/app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,15 +16,9 @@ import { ProductModule } from './products/product.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard] },
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-
-    ], {useHash: true}),
     ProductModule,
+
+    AppRoutingModule
 
   ],
   bootstrap: [AppComponent],
