cec3d6b9ae6ad845009ff49ab8356d6dae9eb38e
Francisco Javier Garcia Muñoz <javidesoft@gmail.com>
2020-12-01 23:14:11 +0100

    Created modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces-pipe';
-import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
@@ -34,6 +27,7 @@ import { ProductDetailGuard } from './products/product-detail.guard';
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
 
     ], {useHash: true}),
+    ProductModule,
 
   ],
   bootstrap: [AppComponent],
